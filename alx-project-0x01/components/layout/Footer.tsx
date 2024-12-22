6f361dceb2fb8c078f5ec943451d369961a3019b
Chiamaka Uyanna <Chiamakauyanna@gmail.com>
2024-12-22 13:03:17 +0100

    add footer content

---
+++ alx-project-0x01/components/layout/Footer.tsx
@@ -0,0 +1,9 @@
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-800 text-white p-4">
+      <p className="text-center">Footer Content</p>
+    </footer>
+  );
+};
+
+export default Footer;
